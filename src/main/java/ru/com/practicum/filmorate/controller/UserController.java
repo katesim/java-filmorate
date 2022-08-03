1b5eac98456d51927e067892f7523b920013e082
katesim <sebastia256@gmail.com>
2022-08-02 21:54:35 -0700

    add UserValidatorTest

--- src/main/java/ru/com/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/com/practicum/filmorate/controller/UserController.java
@@ -3,13 +3,12 @@ package ru.com.practicum.filmorate.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.bind.annotation.*;
 import ru.com.practicum.filmorate.exception.NotFoundException;
 import ru.com.practicum.filmorate.exception.ValidationException;
 import ru.com.practicum.filmorate.model.User;
+import ru.com.practicum.filmorate.validator.UserValidator;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -28,7 +27,7 @@ public class UserController {
 
     @PostMapping(value = "/users")
     public User create(@RequestBody User user) throws ValidationException {
-        userValidate(user);
+        UserValidator.validate(user);
         user.setId(++currId);
         users.put(currId, user);
         log.info("Пользователь с id=" + user.getId() + "создан");
@@ -37,7 +36,7 @@ public class UserController {
 
     @PutMapping(value = "/users")
     public User update(@RequestBody User user) throws ValidationException, NotFoundException {
-        userValidate(user);
+        UserValidator.validate(user);
         if (users.containsKey(user.getId())) {
             users.put(user.getId(), user);
             log.info("Пользователь с id=" + user.getId() + " обновлен");
@@ -45,30 +44,6 @@ public class UserController {
             throw new NotFoundException("Пользователь с id=" + user.getId() + " несуществует");
         }
         return user;
-    }
-
-    private void userValidate(User user) throws ValidationException {
-        if (user.getEmail() == null || user.getEmail().isBlank()) {
-            throw new ValidationException("Адрес электронной почты не может быть пустым");
-        }
-
-        if (!user.getEmail().contains("@")) {
-            throw new ValidationException("Адрес электронной почты должен содержать символ '@'");
-        }
-
-        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
-            throw new ValidationException("Логин не может быть пустым и содержать пробелы");
-        }
-
-        if (LocalDate.parse(user.getBirthday()).isAfter(LocalDate.now())) {
-            throw new ValidationException("Дата рождения не может быть в будущем");
-        }
-
-        if (user.getName() == null || user.getName().isBlank()) {
-            log.warn("Поскольку имя не было передано, вместо него будет использован логин");
-            user.setName(user.getLogin());
-
-        }
     }
 
     @ExceptionHandler(ValidationException.class)
---
+++ src/main/java/ru/com/practicum/filmorate/validator/UserValidator.java
@@ -0,0 +1,34 @@
+package ru.com.practicum.filmorate.validator;
+
+import lombok.extern.slf4j.Slf4j;
+import ru.com.practicum.filmorate.exception.ValidationException;
+import ru.com.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+
+@Slf4j
+public class UserValidator {
+     public static void validate(User user) throws ValidationException {
+        if (user.getEmail() == null || user.getEmail().isBlank()) {
+            throw new ValidationException("Адрес электронной почты не может быть пустым");
+        }
+
+        if (!user.getEmail().contains("@")) {
+            throw new ValidationException("Адрес электронной почты должен содержать символ '@'");
+        }
+
+        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            throw new ValidationException("Логин не может быть пустым и содержать пробелы");
+        }
+
+        if (LocalDate.parse(user.getBirthday()).isAfter(LocalDate.now())) {
+            throw new ValidationException("Дата рождения не может быть в будущем");
+        }
+
+        if (user.getName() == null || user.getName().isBlank()) {
+            log.warn("Поскольку имя не было передано, вместо него будет использован логин");
+            user.setName(user.getLogin());
+        }
+    }
+
+}
