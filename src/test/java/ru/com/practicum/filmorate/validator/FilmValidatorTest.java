14abb9a4bdf51142beeed87ade6a30f1070d2a63
katesim <sebastia256@gmail.com>
2022-08-15 20:51:13 -0700

    fix film tests

--- src/test/java/ru/com/practicum/filmorate/validator/FilmValidatorTest.java
+++ src/test/java/ru/com/practicum/filmorate/validator/FilmValidatorTest.java
@@ -3,9 +3,13 @@ package ru.com.practicum.filmorate.validator;
 import org.junit.jupiter.api.Test;
 import ru.com.practicum.filmorate.exception.ValidationException;
 import ru.com.practicum.filmorate.model.Film;
+import ru.com.practicum.filmorate.model.Genre;
+import ru.com.practicum.filmorate.model.MPARating;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +19,8 @@ class FilmValidatorTest {
     private static final String DESCRIPTION = "my description";
     private static final String RELEASE_DATE = "1999-01-01";
     private static final int DURATION = 150;
+    private static final List<Genre> GENRES = new ArrayList<>();
+    private static final MPARating MPA = new MPARating(1L, "PG");
     private static final String BLANK_STR = "   ";
 
     private static final LocalDate CINEMA_BIRTH = LocalDate.of(1895, 12, 28);
@@ -22,20 +28,20 @@ class FilmValidatorTest {
 
     @Test
     void validate_filmData_isValid() {
-        Film film = new Film(ID, NAME, DESCRIPTION, RELEASE_DATE, DURATION);
+        Film film = new Film(ID, NAME, DESCRIPTION, RELEASE_DATE, DURATION, GENRES, MPA);
         FilmValidator.validate(film);
     }
 
     @Test
     void validate_nameIsEmpty_isNotValid() {
-        Film film = new Film(ID, "", DESCRIPTION, RELEASE_DATE, DURATION);
+        Film film = new Film(ID, "", DESCRIPTION, RELEASE_DATE, DURATION, GENRES, MPA);
 
         assertThrows(ValidationException.class, () -> FilmValidator.validate(film));
     }
 
     @Test
     void validate_nameIsBlank_isNotValid() {
-        Film film = new Film(ID, BLANK_STR, DESCRIPTION, RELEASE_DATE, DURATION);
+        Film film = new Film(ID, BLANK_STR, DESCRIPTION, RELEASE_DATE, DURATION, GENRES, MPA);
 
         assertThrows(ValidationException.class, () -> FilmValidator.validate(film));
     }
@@ -46,7 +52,7 @@ class FilmValidatorTest {
                 "Здесь они хотят разыскать господина Огюста Куглова, который задолжал им деньги, " +
                 "а именно 20 миллионов. о Куглов, который за время " +
                 "«своего отсутствия», стал кандидатом Коломбани.";
-        Film film = new Film(ID, BLANK_STR, longDescription, RELEASE_DATE, DURATION);
+        Film film = new Film(ID, BLANK_STR, longDescription, RELEASE_DATE, DURATION, GENRES, MPA);
 
         assertThrows(ValidationException.class, () -> FilmValidator.validate(film));
     }
@@ -54,21 +60,21 @@ class FilmValidatorTest {
     @Test
     void validate_releaseDateIsTooEarly_isNotValid() {
         String releaseDate = CINEMA_BIRTH.minusDays(1).format(DateTimeFormatter.ISO_DATE);
-        Film film = new Film(ID, BLANK_STR, DESCRIPTION, releaseDate, DURATION);
+        Film film = new Film(ID, BLANK_STR, DESCRIPTION, releaseDate, DURATION, GENRES, MPA);
 
         assertThrows(ValidationException.class, () -> FilmValidator.validate(film));
     }
 
     @Test
     void validate_durationIsNegative_isNotValid() {
-        Film film = new Film(ID, BLANK_STR, DESCRIPTION, RELEASE_DATE, -100);
+        Film film = new Film(ID, BLANK_STR, DESCRIPTION, RELEASE_DATE, -100, GENRES, MPA);
 
         assertThrows(ValidationException.class, () -> FilmValidator.validate(film));
     }
 
     @Test
     void validate_durationIsZero_isNotValid() {
-        Film film = new Film(ID, BLANK_STR, DESCRIPTION, RELEASE_DATE, 0);
+        Film film = new Film(ID, BLANK_STR, DESCRIPTION, RELEASE_DATE, 0, GENRES, MPA);
 
         assertThrows(ValidationException.class, () -> FilmValidator.validate(film));
     }
