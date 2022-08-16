350b804c3379ee7c66be928e172b251fe3013c6f
katesim <sebastia256@gmail.com>
2022-08-15 23:48:18 -0700

    add DBUserStorage

---
+++ src/main/java/ru/com/practicum/filmorate/storage/user/DBUserStorage.java
@@ -0,0 +1,104 @@
+package ru.com.practicum.filmorate.storage.user;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import ru.com.practicum.filmorate.exception.NotFoundException;
+import ru.com.practicum.filmorate.model.User;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Objects;
+
+@Repository("userStorage")
+public class DBUserStorage implements UserStorage {
+    private final JdbcTemplate jdbcTemplate;
+
+    public DBUserStorage(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<User> getAll() {
+        String sqlQuery =
+                "SELECT u.id, " +
+                        "u.email, " +
+                        "u.login, " +
+                        "u.name, " +
+                        "u.birthday, " +
+                "FROM users AS u;";
+        return jdbcTemplate.query(sqlQuery, (rs, rowNum) -> makeUser(rs));
+    }
+
+    @Override
+    public User getById(Long id) {
+        String sqlQuery =
+                "SELECT u.id, " +
+                        "u.email, " +
+                        "u.login, " +
+                        "u.name, " +
+                        "u.birthday, " +
+                "FROM users AS u " +
+                "WHERE u.id = ?;";
+        return jdbcTemplate.query(sqlQuery, (rs, rowNum) -> makeUser(rs), id)
+                .stream()
+                .findAny()
+                .orElseThrow(() -> new NotFoundException("Пользователь с id=" + id + " не существует"));
+    }
+
+    @Override
+    public User add(User user) {
+        String sqlQuery = "INSERT INTO users (email, login, name, birthday) " +
+                "VALUES (?, ?, ?, ?);";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement statement = connection.prepareStatement(sqlQuery, new String[]{"id"});
+            statement.setString(1, user.getEmail());
+            statement.setString(2, user.getLogin());
+            statement.setString(3, user.getName());
+            statement.setDate(4, Date.valueOf(user.getBirthday()));
+            return statement;
+        }, keyHolder);
+
+        user.setId(Objects.requireNonNull(keyHolder.getKey()).longValue());
+        return user;
+    }
+
+    @Override
+    public User update(User user) {
+        String sqlQuery = "UPDATE users " +
+                "SET email = ?, login = ?, name = ?, birthday = ? " +
+                "WHERE id = ?;";
+        jdbcTemplate.update(
+                sqlQuery,
+                user.getEmail(),
+                user.getLogin(),
+                user.getName(),
+                user.getBirthday(),
+                user.getId()
+        );
+        return getById(user.getId());
+    }
+
+    @Override
+    public void delete(User user) {
+        String sqlQuery = "DELETE FROM users WHERE id = ?;";
+        jdbcTemplate.update(sqlQuery, user.getId());
+    }
+
+    private User makeUser(ResultSet rs) throws SQLException {
+        Long id = rs.getLong("id");
+        String email = rs.getString("email");
+        String login = rs.getString("login");
+        String name = rs.getString("name");
+        String birthday = rs.getDate("birthday").toString();
+
+        // TODO брать друзей из таблицы
+        // Set<Long> friends = new HashSet<>();
+        return new User(id, email, login, name, birthday);
+    }
+}
